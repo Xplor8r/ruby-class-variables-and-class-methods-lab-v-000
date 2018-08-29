5cca2c9e11ea116020cd10edada7086c8186ce78
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-29 23:53:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,15 +24,15 @@ class Song
     @@artists.uniq
   end
   def self.genre_count
-    genre_hash = {}
+    genre_count = {}
     @@genres.each do |genre|
-      if genre_hash[genre]
-        genre_hash += 1
+      if genre_count[genre]
+        genre_count += 1
       else
-        genre_hash = 1
+        genre_count = 1
       end
     end
-    genre_hash
+    genre_count
   end
   def self.artist_count
     @artist
