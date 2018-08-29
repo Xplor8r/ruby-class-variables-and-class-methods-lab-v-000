41303e66557b406347aee8795889ef7b25c189c5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-29 23:37:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,11 +18,10 @@ class Song
     @@count
   end
   def self.genres
-    @@genres
+    @@genres.uniq
   end
   def self.artists
-    @@artists ||= []
-    @@artists
+    @@artists.uniq
   end
   def self.genre_count
     @genre.count
