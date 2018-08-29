a3424b3588f1ae4cad4e1b99c5496ba27e26a346
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-29 23:54:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,9 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[ge] += 1
+        genre_count[genre] += 1
       else
-        genre_count = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
