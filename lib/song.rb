93850ca674d98e6349b1c742f8f5bc1dccae5ea6
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-29 23:05:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-
+  @@artists = []
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
-    @artist << artist
+    @artist = artist
     @genre = genre
   end
 
